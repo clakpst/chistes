f4050037b09eb2dcceaa960826c2a657d08dc521
clakpst <117895519+clakpst@users.noreply.github.com>
2022-11-24 10:40:21 +0100

    aa

--- src/Api.js
+++ src/Api.js
@@ -4,4 +4,4 @@ export const getCategories = () =>
   axios.get("https://api.chucknorris.io/jokes/categories");
 
 export const getJokeFromCategory = (category) =>
-  axios.get("https://api.chucknorris.io/jokes/random?category=" + { category });
+  axios.get(`https://api.chucknorris.io/jokes/random?category=${category}`);
--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import { useEffect, useState } from "react";
-import { getCategories } from "./Api";
+import { getCategories, getJokeFromCategory } from "./Api";
 
 export default function App() {
   const [category, setCategory] = useState([]);
+  const [currentCat, setCurrentCat] = useState();
+  const [joke, setJoke] = useState();
 
   useEffect(() => {
     getCategories().then((res) => setCategory(res.data));
   }, []);
 
+  useEffect(() => {
+    getJokeFromCategory(currentCat).then((res) => setJoke(res.data));
+  }, [currentCat]);
+
+  const manejadorClick = (e, cat) => {
+    e.preventDefault();
+    setCurrentCat(cat);
+    console.log(cat);
+  };
+
   return (
     <div>
       <ul>
-        {category.map((cat) => (
-          <li>{cat}</li>
+        {category.map((cat, index) => (
+          <li key={index}>
+            <a href="/#" onClick={(e) => manejadorClick(e, cat)}>
+              {cat}
+            </a>
+          </li>
         ))}
       </ul>
+      <p>{joke && joke.value}</p>
     </div>
   );
 }
